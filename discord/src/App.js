7150c1cb26bb20c25265e1ee5de167b515d63934
Jennifer Stewart <jstewart8053@gmail.com>
2020-10-08 19:22:58 -0500

    created Sidebar and Counter

---
+++ discord/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className='app'>
+      <h1> Lets Build a DISCORD CLONE </h1 >
+    </div>);
+}
+
+export default App;
---
+++ discord/src/Components/Sidebar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "../Sidebar.css";
+
+function Sidebar() {
+    return (
+        <div classNme='sidebar'>
+            <h2> I am the sidebar </h2>
+
+        </div>
+    )
+}
+
+export default Sidebar
