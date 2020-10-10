ddcb46e6db4c9f16762f0be3b643867c9ac94c7c
Jennifer Stewart <jstewart8053@gmail.com>
2020-10-09 21:42:35 -0500

    added component styles

--- discord/src/Components/Sidebar.js
+++ discord/src/Components/Sidebar.js
@@ -4,7 +4,13 @@ import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import AddIcon from '@material-ui/icons/Add';
 import SidebarChannel from './SidebarChannel';
 import SignalCellularAltIcon from '@material-ui/icons/SignalCellularAlt';
-
+import InfoOutlinedIcon from '@material-ui/icons/InfoOutlined';
+import CallIcon from '@material-ui/icons/Call';
+import { Avatar } from '@material-ui/core';
+import MicIcon from '@material-ui/icons/Mic';
+import HeadsetIcon from '@material-ui/icons/Headset';
+import SettingsIcon from '@material-ui/icons/Settings';
+import SVG from '../avatar.png';
 
 function Sidebar() {
     return (
@@ -32,9 +38,36 @@ function Sidebar() {
                 </div>
             </div>
             <div className="sidebar_voice">
-                <SignalCelluarAltIcon
+                <SignalCellularAltIcon
                     className='sidebar-voiceIcon'
-                    fontSize='large' />
+                    fontSize='large'
+                />
+
+                <div className="sidebar_voiceInfo">
+                    <h3>Voice Connected </h3>
+                    <p>Stream </p>
+                </div>
+
+                <div className="sidebar_voiceIcons">
+                    <InfoOutlinedIcon />
+                    <CallIcon />
+
+                </div>
+                <div className="sidebar_profile">
+                    <Avatar src={SVG} />
+                    <div className="sidebar_profileInfo">
+                        <h3>JenniferStewart</h3>
+                        <p>#jstewartDiscord</p>
+                    </div>
+                    <div className="sidebar_profileIcons">
+                        <MicIcon />
+                        <HeadsetIcon />
+                        <SettingsIcon />
+
+                    </div>
+                </div>
+
+
             </div>
         </div>
     )
